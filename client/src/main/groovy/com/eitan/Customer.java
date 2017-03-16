280f58e4c353d169d3afc6909389fe0e3bf64d6d
Eitan Suez <eitan.suez@gmail.com>
2017-03-16 19:37:01 -0400

    whatevs

--- client/src/main/groovy/com/eitan/Customer.java
+++ client/src/main/groovy/com/eitan/Customer.java
@@ -18,20 +18,9 @@ public class Customer {
         '}';
   }
 
-  public String getLastName() {
-    return lastName;
-  }
+  public String getLastName() { return lastName; }
+  public void setLastName(String lastName) { this.lastName = lastName; }
 
-  public void setLastName(String lastName) {
-    this.lastName = lastName;
-  }
-
-  public String getFirstName() {
-
-    return firstName;
-  }
-
-  public void setFirstName(String firstName) {
-    this.firstName = firstName;
-  }
+  public String getFirstName() { return firstName; }
+  public void setFirstName(String firstName) { this.firstName = firstName; }
 }
